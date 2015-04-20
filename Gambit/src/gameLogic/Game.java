8b8549f3415aecb7a354e8a7f74ef84c166050fc
Kelvin Lu <kelvin.lu.sj@gmail.com>
2015-04-19 19:41:12 -0700

    Initial commit. Project proposal submitted as HTML.

---
+++ Gambit/src/gameLogic/Game.java
@@ -0,0 +1,12 @@
+package gameLogic;
+
+/**
+ * A Game object is an instance of a game between two Players. Keeps track of 
+ * the current state of the game.
+ * @author KelvinLu
+ *
+ */
+public class Game
+{
+   
+}
